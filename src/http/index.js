c95ebedcb84aeb6dd38333095dfe4210e3af9577
Vadim Hmys <hmyzvadim@yandex.ru>
2024-09-06 20:47:23 +0300

    Update index.js

--- src/http/index.js
+++ src/http/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 //import config from 'dotenv/config';
 
 const guestInstance = axios.create({
-  baseURL: 'https://notesbe-0033cba62263.herokuapp.com/',
+  baseURL: 'https://rocky-garden-32562-a0a3b57853f8.herokuapp.com/',
 });
 
 export { guestInstance };
